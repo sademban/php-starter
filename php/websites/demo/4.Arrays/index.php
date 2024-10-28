877233e36f585b31ec83581f41b51635f9bbfac4
nabin-motocle <n.sademba@motocle.com>
2024-10-28 18:03:22 +0900

    Arrays

---
+++ php/websites/demo/4.Arrays/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Demo</title>
+</head>
+<body>
+    <h1> Recommended Books</h1>
+    <?php 
+        $books = [
+            "Do Android Drreams of Electric Sheep", 
+            "The Langoliers", 
+            "Hail Mary"
+        ];
+    ?>
+
+    <ul>
+        <!-- <?php 
+            foreach ($books as $book) {
+                echo "<li>{$book}</li>";
+            }
+        ?> -->
+        
+        <?php foreach ($books as $book): ?>
+            <li><?= $book ?></li>
+        <?php endforeach; ?>
+    </ul>
+</body>
+</html>
