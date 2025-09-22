7158ca0ca240458badc41423d45fe4d7829817df
sademban <nabinsademba@gmail.com>
2025-09-22 23:15:22 +0900

    3,4

--- php/websites/demo/2.Dynamic Web Applicaitons/4.Make a PHP Router/views/404.php
+++ php/websites/demo/2.Dynamic Web Applicaitons/4.Make a PHP Router/views/404.php
@@ -1,10 +1,12 @@
 <?php require 'partials/head.php'; ?>
 <?php require 'partials/nav.php'; ?>
-<?php require 'partials/banner.php'; ?>
 
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <p>Hello, welcome to the contact page</p>
+      <h1 class="text-2xl font-bold">Sorry, page not found</h1>
+      <p class="mt-4">
+        <a href="/" class="text-blue underline">Go back to home</a>
+      </p>
     </div>
   </main>
   
--- php/websites/demo/2.Dynamic Web Applicaitons/5.Create a MySQL Database/views/about.view.php
+++ php/websites/demo/2.Dynamic Web Applicaitons/5.Create a MySQL Database/views/about.view.php
@@ -4,7 +4,7 @@
 
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <p>Hello, welcome to the contact page</p>
+      <p>Hello, welcome to the about page</p>
     </div>
   </main>
   
